abbf2ac7ac548318be44f46ad53acf298df2b4d0
soldatviktimnosti <tishkinsergey123@gmail.com>
2025-09-26 22:17:44 +0300

    Обновил формат конфига

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from 'eslint-plugin-js';
+
+export default [
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'commonjs',
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                require: 'readonly',
+                module: 'readonly',
+                exports: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly',
+                describe: 'readonly',
+                it: 'readonly',
+                test: 'readonly',
+                expect: 'readonly',
+                beforeAll: 'readonly',
+                afterAll: 'readonly',
+                beforeEach: 'readonly',
+                afterEach: 'readonly'
+            }
+        },
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-console': 'off'
+        }
+    }
+];
